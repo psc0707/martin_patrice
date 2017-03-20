f25494b1f61aa4c5bbf8167392f60547ea2a319b
LUX3-16\Etudiant <martin.stoz@gmail.com>
2017-03-20 15:29:44 +0100

    Revert "Revert "config.php -> Removed Whitespaces""
    This reverts commit 9771f6529f29b02dd7cf75cd29849f473c9825ad.

--- inc/config.php
+++ inc/config.php
@@ -8,7 +8,6 @@ define('DB_DATABASE', 'patrices_sql0');
 
 // définition DSN
 $dsn = 'mysql:dbname='.DB_DATABASE.';host='.DB_HOST.';charset=UTF8';
-print_r($dsn);
 // Instanciation de PDO
 try {
 	$pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
@@ -16,19 +15,5 @@ try {
 catch (Exception $e) {
 	echo $e->getMessage();
 }
-
-//
-//// Charge tous les packages Composer
-//require 'vendor/autoload.php';
-//
-//use SocialLinks\Page;
-//
-////Create a Page instance with the url information
-//$socialPage = new Page([
-//    'url' => 'http://localhost'.$_SERVER['REQUEST_URI'],
-//    'title' => 'Projet toto',
-//    'text' => 'Extended page description',
-//    'twitterUser' => '@toto'
-//]);
 
 require dirname(dirname(__FILE__)).'/inc/functions.php';
