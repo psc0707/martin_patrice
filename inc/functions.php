b7df8c21962166a6d64cfb30626aa6b76304ff00
LUX3-16\Etudiant <martin.stoz@gmail.com>
2017-03-20 17:03:45 +0100

    Functions -> Added Constants and SQL Query Index -> Removed Functions and added include into Config.php Home -> Added and changed view

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,11 @@
 <?php
+	// Defining Constants for omdb api queries
+	define("omdbApi_title", "http://www.omdbapi.com/?t=");
 
-
-
-
-
+	// Query to fetch 4 latest movies
+	$newestAddition = 'SELECT *
+	FROM movies
+	LEFT OUTER JOIN categories ON categories.cat_id = movies.categories_cat_id
+	LIMIT 4
+	ORDER BY movies.mov_id DESC';
+?>
--- view/home.php
+++ view/home.php
@@ -1,39 +1,25 @@
-<?php foreach ($sessionsList as $currentLocation=>$currentSessionsList) :?>
-	<div class="panel panel-primary">
-	  <div class="panel-heading">
-	    <h3 class="panel-title"><?= $currentLocation ?></h3>
-	  </div>
-		<!-- Table -->
-		<table class="table">
-		<thead>
-			<tr>
-				<th></th>
-				<th>Début</th>
-				<th>Fin</th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php foreach ($currentSessionsList as $currentRow) : ?>
-			<tr>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>">#<?= $currentRow['ses_number'] ?></a></td>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>"><?= $currentRow['ses_start_date'] ?></a></td>
-				<td><a href="list.php?sesId=<?= $currentRow['ses_id'] ?>"><?= $currentRow['ses_end_date'] ?></a></td>
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
-		</table>
-	</div>
-<?php endforeach; ?>
+<div class="panel panel-primary">
+  <div class="panel-heading"><h3 class="panel-title">Introduction</h3></div>
+	<!-- Table -->
+	<label>This Project has been created by Patrice, Martin and Richard. The Webpage uses the OMDb API via CRUD in order to receive further Details</label>
+</div>
+
+<form>
+	<input type="text" name="search" id="search_movies" placeholder="Search">
+	<input type="hidden" name="search_form" value="1">
+	<input type="submit" name="submit_form" value="Search">
+</form><br>
 
 <div class="panel panel-primary">
-	<div class="panel-heading">
-		<h3 class="panel-title">Statistiques sur le nombre d'étudiants par ville</h3>
-	</div>
+	<div class="panel-heading"><h3 class="panel-title">Newest Additions</h3></div>
+	<!-- Table -->
 	<table class="table">
 	<thead>
 		<tr>
-			<th>Ville</th>
-			<th>Nombre d'étudiants</th>
+			<th>Movie 1</th>
+			<th>Movie 2</th>
+			<th>Movie 3</th>
+			<th>Movie 4</th>
 		</tr>
 	</thead>
 	<tbody>
