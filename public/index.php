ba231205608f794d5d13100737c3a2ca42b61de5
psc <psc0707@gmail.com>
2017-03-20 15:24:54 +0100

    reup

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 // Inclusion de config.php
 require dirname(dirname(__FILE__)).'/inc/config.php';
+require dirname(dirname(__FILE__)).'/inc/functions.php';
 
  // Récupération de données ou autres
+
 
 
 // A la fin, toujours les vues
