9b3c7ed0a6a63fbb31671cf0ed1e66f4278cc1cc
psc0707 <psc0707@gmail.com>
2017-03-21 23:37:09 +0100

    Add files via upload

---
+++ js/crud.js
@@ -0,0 +1,67 @@
+var imdb_data;
+
+$(document).ready(function() {
+
+// Pré-remplir un film via Imdb
+//-------------------------------
+	$("#imdb" ).on( "submit", function( event ) {		
+		event.preventDefault();
+		// searchMovie = $( this ).serialize();
+		searchMovie = $('#imdb input').val();
+		// console.log(searchMovie);
+	    $.ajax({
+	        method: "GET",
+	        url: "http://www.omdbapi.com",
+	        data: {
+	              t : searchMovie
+	            },
+	        dataType : 'json'
+	        })
+	        .done(function(response) {	  		        	
+	            // console.log(response);
+	            // console.log('response.genre '+response.Genre);
+	            $("input[name*='cat_name']").val(response.Genre);
+	            $("input[name*='mov_title']").val(response.Title);
+	            $("input[name*='mov_released']").val(response.Released);
+	            $("input[name*='mov_language']").val(response.Language);
+	            $("input[name*='mov_actors']").val(response.Actors);
+	            $("textarea[name*='mov_synopsis']").val(response.Plot);
+	            $("input[name*='mov_poster']").val(response.Poster);
+	          })
+	        .fail(function() {
+	            console.log( "bad news... ERROR !" );
+	        });
+	});
+
+// Ajout/update d'un film en base
+//-------------------------------
+	$("#crudForm" ).on( "submit", function( event ) {		
+		event.preventDefault();
+		movieInfos = $( this ).serialize();
+		
+		// console.log(movieInfos);
+	    $.ajax({
+	        method: "POST",
+	        url: "ajax/crud.php",
+	        data: {
+	              movieInfos
+	            },
+	        dataType : 'json'
+	        })
+	        .done(function(response) {	  		        	
+	            // console.log(response);
+	            if (response.code != 0) {
+	            // console.log('response.genre '+response.Genre);
+	            	$("textarea").val("");
+	            	$("input").val("");
+	            } else {
+	            	console.log(response.errorList);
+	            }
+
+	          })
+	        .fail(function() {
+	            console.log( "bad news... ERROR !" );
+	        });
+	});
+
+});
