f554cd2645ed20c7d4c1fc61d9b38bf61ef9aca6
psc0707 <psc0707@gmail.com>
2017-03-21 23:35:52 +0100

    Add files via upload

--- view/add-movie.php
+++ view/add-movie.php
@@ -1,14 +1,22 @@
 <div class="panel panel-primary">
   <div class="panel-heading">
     <h3 class="panel-title">
-    <?php if (!empty($studentInfos['stu_id'])) : ?>
-    	Modification de <?= $studentInfos['stu_firstname'] ?> <?= $studentInfos['stu_lastname'] ?>
+    <?php if (!empty($movieDetails['mov_id'])) : ?>
+    	Modification de <?= $movieDetails['mov_title'] ?>
     <?php else : ?>
-    	Ajout d'un étudiant
+    	Add Movies
     <?php endif; ?>
     </h3>
   </div>
   <div class="panel-body">
+
+	<form id="imdb" action="" class="navbar-form navbar-left" method="get">
+		<div class="form-group">
+			<input type="text" name="searchImdb" class="form-control" placeholder="Search">
+		</div>
+		<button type="submit" class="btn btn-success btn-sm">Add from Imdb</button>
+	</form>
+
   	<?php if (!empty($errorList)) : ?>
 		<div class="alert alert-danger" role="alert">
 		  <?php foreach ($errorList as $currentErrorText) : ?>
@@ -16,77 +24,70 @@
 		  <?php endforeach; ?>
 		</div>
   	<?php endif; ?>
-	<form action="" method="post" enctype="multipart/form-data">
-		<?php if (!empty($studentInfos['stu_image'])) :?>
-			<img src="<?= $studentInfos['stu_image'] ?>" alt="" height="140" style="display:block;margin:auto;" /><br>
-		<?php endif; ?>
+	<form id="crudForm" action="" method="post">
   		<div class="row">
-	  		<div class="col-md-6 col-sm-6 col-xs-12">
+	  		<div class="col-md-12 col-sm-12 col-xs-12">
+	  		
+				<div class="form-group">
+					<label>Genre</label>
+					<input type="text" class="form-control" name="cat_name" value="<?= $movieDetails['cat_name'] ?>" placeholder="Genre" />
+				</div>
+				
+				<div class="form-group">
+					<label>Title</label>
+					<input type="text" class="form-control" name="mov_title" value="<?= $movieDetails['mov_title'] ?>" placeholder="Title" />
+				</div>
 
 				<div class="form-group">
-					<label>Nom</label>
-					<input type="text" class="form-control" name="stu_lastname" value="<?= $studentInfos['stu_lastname'] ?>" placeholder="Nom" />
+					<label>Annee</label>
+					<input type="text" class="form-control" name="mov_released" value="<?= $movieDetails['mov_released'] ?>" placeholder="" />
 				</div>
-				
-				<div class="form-group">
-					<label>Prénom</label>
-					<input type="text" class="form-control" name="stu_firstname" value="<?= $studentInfos['stu_firstname'] ?>" placeholder="Prénom" />
-				</div>
-				
-				<div class="form-group">
-					<label>Email</label>
-					<input type="text" class="form-control" name="stu_email" value="<?= $studentInfos['stu_email'] ?>" placeholder="Email" />
-				</div>
-				
-				<div class="form-group">
-					<label>Date de naissance</label>
-  					<input type="text" class="form-control" name="stu_birthdate" value="<?= $studentInfos['stu_birthdate'] ?>" placeholder="Date de naissance" />
-  					<small class="form-text text-muted">au format YYYY-MM-DD (<?= date('Y-m-d') ?>)</small>
-  				</div>
-			</div>
-	  		<div class="col-md-6 col-sm-6 col-xs-12">
 
 				<div class="form-group">
-					<label>Sympathie</label>
-					<select name="stu_friendliness" class="form-control">
-						<option value="">choisissez</option>
-						<?php for($i=0;$i<=5;$i++) : ?>
-							<option value="<?= $i ?>"<?= $studentInfos['stu_friendliness'] == $i ? ' selected=selected"' : '' ?>><?= getSympathieName($i) ?></option>
-						<?php endfor; ?>
-					</select>
+					<label>Langue</label>
+					<input type="text" class="form-control" name="mov_language" value="<?= $movieDetails['mov_language'] ?>" placeholder="" />
+				</div>
+
+				<div class="form-group">
+					<label>Acteurs</label>
+					<input type="text" class="form-control" name="mov_actors" value="<?= $movieDetails['mov_actors'] ?>" placeholder="" />
+				</div>
+
+				<div class="form-group">
+					<label>Synopsis</label>
+					<textarea name="mov_synopsis" class="form-control" rows="4">	
+						<?= $movieDetails['mov_synopsis'] ?>					
+					</textarea>					
+				</div>
+
+				<div class="form-group">
+					<label>Poster</label>
+					<input type="text" class="form-control" name="mov_poster" value="<?= $movieDetails['mov_poster'] ?>" placeholder="" />
+				</div>
+
+				<div class="form-group">
+					<label>Link</label>
+					<input type="text" class="form-control" name="mov_link" value="<?= $movieDetails['mov_link'] ?>" placeholder="" />
 				</div>
 				
 				<div class="form-group">
-					<label>Session</label>
-					<select name="ses_id" class="form-control">
-						<option value="">choisissez</option>
-						<?php foreach ($sessionsList as $sesId=>$sesName) : ?>
-							<option value="<?= $sesId ?>"<?= $studentInfos['session_ses_id'] == $sesId ? ' selected=selected"' : '' ?>><?= $sesName ?></option>
-						<?php endforeach; ?>
-					</select>
-				</div>
-				
-				<div class="form-group">
-					<label>Ville</label>
-					<select name="cit_id" class="form-control">
-						<option value="">choisissez</option>
-						<?php foreach ($citiesList as $citId=>$citName) : ?>
-							<option value="<?= $citId ?>"<?= $studentInfos['city_cit_id'] == $citId ? ' selected=selected"' : '' ?>><?= $citName ?></option>
-						<?php endforeach; ?>
-					</select>
-				</div>
-				
-				<div class="form-group">
-					<label>Image</label>
-					<input type="file" name="stu_image" placeholder="Image" />
-				</div>
+					<label>Support</label>
+  					<input type="text" class="form-control" name="mov_support" value="<?= $movieDetails['mov_support'] ?>" placeholder="" />  					
+  				</div>							
+
+				<?php if (!empty($movieDetails['mov_id'])) : ?>
+					<input type="submit" class="btn btn-success btn-block" value="Modifier" />
+		    	<?php else : ?>
+					<input type="submit" class="btn btn-success btn-block" value="Ajouter" />
+				<?php endif; ?>
+
 			</div>
-		</div>
-		<?php if (!empty($studentInfos['stu_id'])) : ?>
-			<input type="submit" class="btn btn-success btn-block" value="Modifier" />
-    	<?php else : ?>
-			<input type="submit" class="btn btn-success btn-block" value="Ajouter" />
-		<?php endif; ?>
+		
+
+		</div>		
 	</form>
   </div>
 </div>
+
+<!-- Appel Ajax pour le CRUD -->
+<script type="text/javascript" src="./js/crud.js"></script>
