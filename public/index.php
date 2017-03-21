e93550cefe6f8cce0f460bb9d52f7dfb117b7530
psc <psc0707@gmail.com>
2017-03-21 16:47:44 +0100

    BugFix

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 // Inclusion de config.php
 require dirname(dirname(__FILE__)).'/inc/config.php';
-require dirname(dirname(__FILE__)).'/inc/functions.php';
 
 // Récupération de données ou autres
 
