4f11a1d6fd14826d9cb87c7d239b3bce094df6c2
LUX3-16\Etudiant <martin.stoz@gmail.com>
2017-03-22 12:28:33 +0100

    done

--- public/add-category.php
+++ public/add-category.php
@@ -3,28 +3,56 @@
 require dirname(dirname(__FILE__)).'/inc/config.php';
 
 $newCatName = '';
+
 $modCatName = '';
+
+$sql = '
+		SELECT cat_name, cat_id
+		FROM categories
+	';
+
+	$results = $pdo->query($sql);
+	$resultList = $results->fetchAll(PDO::FETCH_ASSOC); // 
+	print_r($resultList);
+
+
 
 if (!empty($_POST)) {
 	// print_r($_POST);
 	$newCatName = strip_tags(trim($_POST['newCategory']));
 	// echo $newCatName;
+
+	$sql = '
+		INSERT INTO categories (cat_name)
+		VALUES (:catName)
+	';
+	$sth = $pdo->prepare($sql);
+	$sth->bindValue(':catName', $modCatName);
+	if ($sth->execute() === false) {
+			print_r($sth->errorInfo());
+		}
+	else {
+		// Je récupère l'ID auto-incrémenté
+		return $pdo->lastInsertId();
+	}	
 }
 
 if (!empty($_GET)) {
-	// print_r($_GET);
+	print_r($_GET);
 	$modCatName = strip_tags(trim($_GET['modifyCategory']));
-	// echo $modCatName;
+	echo $modCatName;
+
+	$sql = '
+		UPDATE categories
+		SET cat_name = :catName
+		WHERE cat_id = :catId
+	';
+	$sth = $pdo->prepare($sql);
+	$sth->bindValue(':id', $id);
+	$sth->bindValue(':lastname', $lastname);
+
 }
 
-$sql = '
-	SELECT cat_name
-	FROM categories
-';
-
-$results = $pdo->query($sql);
-$resultList = $results->fetchAll(PDO::FETCH_ASSOC); // 
-print_r($resultList);
 
 
 
